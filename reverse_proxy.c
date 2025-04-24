243fef826739b1c93b8e58734dff4673c1dcd0a8
SanjayKumar <0508sanjaykumar@gmail.com>
2025-04-24 10:26:03 +0530

    Base commit

---
+++ reverse_proxy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>             // for close()
+#include <arpa/inet.h>          // for inet_addr, sockaddr_in
+
+#define PORT 8080
+#define BUFFER_SIZE 4096
+
+int main()
+{
+    printf("hello world");
+    return 0;
+}
